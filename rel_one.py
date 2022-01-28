b541d32302a18e689fd7bfb975c492877db3037d
Ronald Cesar Dias de Oliveira <ronald@debian.ronald>
2022-01-28 20:53:26 -0300

    :recycle: Refactoring the code

--- rel_one.py
+++ rel_one.py
@@ -10,14 +10,7 @@ def equivalent_resistor(*r_args):
     req = rm/rp
     return req
 
-def current_divisor(i, rx, *r_args):
-    rm,rp = 1,0
-    if len(r_args) <= 0:
-        return
-    for c in r_args:
-        rm *= c
-        rp += c
-    req = rm/rp
+def current_divisor(i, rx, req):
     return (i*req/(rx+req))
 
 I_1 = 0.80*mt.pow(10,-3)
@@ -26,9 +19,9 @@ R_2 = 72*mt.pow(10,3)
 R_3 = 0.4*mt.pow(10,3)
 V = equivalent_resistor(equivalent_resistor(R_1, R_2), R_3) * I_1
 
-I_r1 = current_divisor(I_1, R_1, R_2, R_3)
-I_r2 = current_divisor(I_1, R_2, R_1, R_3)
-I_r3 = current_divisor(I_1, R_3, R_2, R_1) 
+I_r1 = current_divisor(I_1, R_1, equivalent_resistor(R_2, R_3))
+I_r2 = current_divisor(I_1, R_2, equivalent_resistor(R_1, R_3))
+I_r3 = current_divisor(I_1, R_3, equivalent_resistor(R_2, R_1)) 
 
 I_tot = I_r1 + I_r2 + I_r3
 P_r1 = mt.pow(I_r1,2) * R_1
