9bb8d6f00535a0319c7afc5a83f617d243498649
Ronald Cesar Dias de Oliveira <ronald@debian.ronald>
2022-01-28 19:24:50 -0300

    :wrench: Adding the voltage value

--- rel_one.py
+++ rel_one.py
@@ -1,5 +1,15 @@
 import math as mt
 
+def equivalent_resistor(*r_args):
+    rm,rp = 1,0
+    if len(r_args) <= 0:
+        return
+    for c in r_args:
+        rm *= c
+        rp += c
+    req = rm/rp
+    return req
+
 def current_divisor(i, rx, *r_args):
     rm,rp = 1,0
     if len(r_args) <= 0:
@@ -14,6 +24,7 @@ I_1 = 0.80*mt.pow(10,-3)
 R_1 = 0.5*mt.pow(10,3)
 R_2 = 72*mt.pow(10,3)
 R_3 = 0.4*mt.pow(10,3)
+V = equivalent_resistor(equivalent_resistor(R_1, R_2), R_3) * I_1
 
 I_r1 = current_divisor(I_1, R_1, R_2, R_3)
 I_r2 = current_divisor(I_1, R_2, R_1, R_3)
@@ -25,6 +36,7 @@ P_r2 = mt.pow(I_r2,2) * R_2
 P_r3 = mt.pow(I_r3,2) * R_3
 P_tot = P_r1 + P_r2 + P_r3
 
+print(f"V = {V} V")
 print (f"Ir1 = {I_r1} A")
 print (f"Ir2 = {I_r2} A")
 print (f"Ir3 = {I_r3} A")
